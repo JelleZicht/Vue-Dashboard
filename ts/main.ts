a32f5f3e9a29c8021bdded11e8dd2a2b8f954956
WhoKnows007 <jelle_jelle42@hotmail.com>
2019-11-22 23:47:49 +0100

    updated package json and project confirmed fixed

--- ts/main.ts
+++ ts/main.ts
@@ -8,3 +8,12 @@ window.onload = function () {
         }
     });
 }
+/*
+
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+document.body.textContent = greeter(user);*/
